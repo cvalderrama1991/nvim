292f1e12f016e78c0aa1f1e0931f82ded8f437fb
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-09-24 22:59:15 -0400

    nvim colorscheme set to retrobox

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 -- ================================================================================================
 
 -- theme & transparency
--- vim.cmd.colorscheme("unokai")
+vim.cmd.colorscheme("retrobox")
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
